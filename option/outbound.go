xin0aini/sing-box

Add SideLoad Support

option/outbound.go
@@ -23,6 +23,7 @@ type _Outbound struct {
 	ShadowTLSOptions    ShadowTLSOutboundOptions    `json:"-"`
 	ShadowsocksROptions ShadowsocksROutboundOptions `json:"-"`
 	VLESSOptions        VLESSOutboundOptions        `json:"-"`
+	SideLoadOptions     SideLoadOutboundOptions     `json:"-"`
 	SelectorOptions     SelectorOutboundOptions     `json:"-"`
 	URLTestOptions      URLTestOutboundOptions      `json:"-"`
 	RandomOptions       RandomOutboundOptions       `json:"-"`
@@ -61,6 +62,8 @@ func (h Outbound) MarshalJSON() ([]byte, error) {
 		v = h.ShadowsocksROptions
 	case C.TypeVLESS:
 		v = h.VLESSOptions
+	case C.TypeSideLoad:
+		v = h.SideLoadOptions
 	case C.TypeSelector:
 		v = h.SelectorOptions
 	case C.TypeURLTest:
@@ -108,6 +111,8 @@ func (h *Outbound) UnmarshalJSON(bytes []byte) error {
 		v = &h.ShadowsocksROptions
 	case C.TypeVLESS:
 		v = &h.VLESSOptions
+	case C.TypeSideLoad:
+		v = &h.SideLoadOptions
 	case C.TypeSelector:
 		v = &h.SelectorOptions
 	case C.TypeURLTest:

in-out/sing-box-ref

option/outbound.go
@@ -25,6 +25,7 @@ type _Outbound struct {
 	VLESSOptions        VLESSOutboundOptions        `json:"-"`
 	TUICOptions         TUICOutboundOptions         `json:"-"`
 	Hysteria2Options    Hysteria2OutboundOptions    `json:"-"`
+	SideLoadOptions     SideLoadOutboundOptions     `json:"-"`
 	SelectorOptions     SelectorOutboundOptions     `json:"-"`
 	URLTestOptions      URLTestOutboundOptions      `json:"-"`
 }
@@ -66,6 +67,8 @@ func (h *Outbound) RawOptions() (any, error) {
 		rawOptionsPtr = &h.TUICOptions
 	case C.TypeHysteria2:
 		rawOptionsPtr = &h.Hysteria2Options
+	case C.TypeSideLoad:
+		rawOptionsPtr = &h.SideLoadOptions
 	case C.TypeSelector:
 		rawOptionsPtr = &h.SelectorOptions
 	case C.TypeURLTest: